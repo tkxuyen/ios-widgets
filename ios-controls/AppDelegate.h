db00315827ba8dc45ec9d835b13ad5468052b1b1
TKXuyen <tkxuyen@gmail.com>
2012-02-19 12:36:12 +0700

    Initial Commit

---
+++ ios-controls/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ios-controls
+//
+//  Created by tran khai xuyen on 2/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ios-controls/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ios-controls
+//
+//  Created by tran khai xuyen on 2/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
